5ffe6b14db2576cd05fa08af81adabfb51ce0b93
AdrianHsu <adrianhsu1995@gmail.com>
2015-07-11 15:51:37 +0800

    ?

--- app/src/main/java/com/example/adrianhsu/usagemonitor/MainActivity.java
+++ app/src/main/java/com/example/adrianhsu/usagemonitor/MainActivity.java
@@ -1,17 +1,46 @@
 package com.example.adrianhsu.usagemonitor;
 
+import java.util.List;
+
+
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import android.content.Context;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
+import android.widget.Toast;
+import android.util.Log;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final String ACTIVITY_TAG="MainActivity";
+    private static final int MAX_RUNNING = 1000;
+    private ActivityManager am;
+    private ActivityManager.RunningServiceInfo serviceInfo;
+    private List<RunningServiceInfo> runningServiceInfoList;
+    Context context;
+
+    void createRecorder() {
+
+        Log.v(Context.ACTIVITY_SERVICE, "test");
+        am = (ActivityManager) context
+                .getSystemService(Context.ACTIVITY_SERVICE);
+
+        serviceInfo = new RunningServiceInfo();
+        runningServiceInfoList = am.getRunningServices(MAX_RUNNING);
+
+        Log.v(this.ACTIVITY_TAG, "This is Verbose.");
+    }git
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createRecorder();
     }
 
     @Override
